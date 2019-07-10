713d46ccbe667d0a4dfcaa52df6ecca726121a40
DimasBo <dimasbo.work@gmail.com>
2019-07-11 00:06:21 +0300

    beta

--- svitlana_chit/settings.py
+++ svitlana_chit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = None
+SECRET_KEY = '&m26lkw_snxs$g=$qjkv$7(i1o8m)wm^_=5v*uyt)$hp7d948('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'svitlana_chit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sveta_db',
+        'USER':'sveta',
+        'PASSWORD':'sveta_09_shop',
+        # 'HOST':'127.0.0.1',
+        # 'PORT':'5432',
     }
 }
 
