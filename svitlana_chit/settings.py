d939afdd791d313fbb78318b0dc4ea35da6109d4
DimasBo <dimasbo.work@gmail.com>
2019-07-13 16:15:52 +0300

    heroku f

--- svitlana_chit/settings.py
+++ svitlana_chit/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&m26lkw_snxs$g=$qjkv$7(i1o8m)wm^_=5v*uyt)$hp7d948('
+#SECRET_KEY = '&m26lkw_snxs$g=$qjkv$7(i1o8m)wm^_=5v*uyt)$hp7d948('
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&m26lkw_snxs$g=$qjkv$7(i1o8m)wm^_=5v*uyt)$hp7d948(')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 #DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
 ALLOWED_HOSTS = ['*']
@@ -50,6 +52,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,21 +140,24 @@ USE_L10N = True
 
 USE_TZ = True
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static_in_dev')
-]
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+#STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, 'static_in_dev')
+#]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
